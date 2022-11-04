8c2497ad57c670a1f2f91e1f908ea9a0f822722a
237nkanke <92725190+237nkanke@users.noreply.github.com>
2022-11-04 16:39:27 +0100

    first commit

---
+++ src/component/Navbar/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Nav, NavbarContainer , NavLogo} from './NavbarElement'
+const Navbar = () => {
+  return (
+    <>
+    
+    <Nav>
+      <NavbarContainer>
+        <NavLogo to="/">
+            dolla
+        </NavLogo>
+        </NavbarContainer>  
+    </Nav>
+    </>
+  )
+}
+
+export default Navbar
